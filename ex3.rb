58196a93137fcc5968fba32c3f4e9a11853ebca8
C Allgood <bplandesign@gmail.com>
2015-03-08 15:17:21 -0400

    Commit message

---
+++ ex3.rb
@@ -0,0 +1,31 @@
+#types the sentence below
+puts "I will now count my chickents:"
+
+#Does a calculation but not sure why the hashtag was added
+puts "Hens #{25.0+30.0/6.0}"
+
+
+# Types the Roosters and then subtracts mulitplies 25x3 and subtracts from a 100? uses PEMDA-PEMDAS which stands for Parentheses Exponents Multiplication Division Addition Subtraction. That's the order Ruby follows as well.
+puts "Roosters #(100.0-25.0 * 3.0 % 4.0}"
+
+
+#types sentence
+puts "Now I will count the  egss:"
+
+#Adds up some stuff, still not sure about that modulus
+puts 3+2+1-5+4 %2-1 /4 +6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3+2<5-7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5-7}"
+puts "Oh, that's why its's false."
+
+
+puts "How about some more."
+
+puts "Is it greater? #{5>= -2}"
+puts "Is it greater or equal? #{5.+ -2}"
+puts "Is it less or equal? #(5,= -2}"
