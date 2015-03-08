ebe80c7433acdb0b9086b2906f4dd9ef0275f37e
C Allgood <bplandesign@gmail.com>
2015-03-08 15:37:15 -0400

    Commitmessage

---
+++ ex7.rb
@@ -0,0 +1,28 @@
+#prints the following
+puts "Mary had a little lamb."
+
+puts "Its fleece was white as #{'sno'}."
+
+puts "And everywhere that Mar4y went."
+
+puts "." * 1- # wha'd that do?
+
+
+# Allof the following are variables...imagine this type above each one.
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch tht pring vs. pus on this line what's it do?
+# I guess print sent the Cheese and PUTS added the Burger to the Cheese
+print end1 + end2 + end3 + end4 + end5 + end6 
+puts end7 + end8 + end9 + end11  + end12
