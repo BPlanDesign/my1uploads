89656b92a59e4c7271f54ced503f430f51d2548e
C Allgood <bplandesign@gmail.com>
2015-03-08 15:35:36 -0400

    Commit message

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+#This is a variable
+types_of_people + 10
+#This a variable that is equal to a string which is a line of text we want to show somebody or export out.
+x = "There are # {types_of people} types of people."
+#this is a variable
+binary = "binary"
+#this is a variable
+do_not = "don't"
+#This is a variable that points to a string INSIDE of a string.
+y = "Those who know #{binary} and those who # {do_not}."
+
+#this just tells the program to print the variable x (HOW DO YOU KNOW X IS A VARIABLE WITH OUT LOOKING UP)
+puts x
+#see above
+puts Y
+#This prints a VARIABLE that is inside a STRING
+puts "I said: '#{x}."
+#See above
+puts "I also said: '#{y}'."
+
+#This is a variable
+hilarious = false
+#This is a variable that is a STRING that has a variable INSIDE of it
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#This just prints the variable that comes out as a string with a variable
+puts joke_evaluation
+
+#This is a variable that is a string
+w = "This is the left side of ..."
+#This is a variable that is a string
+e = "a string with a right side."
+
+
+#This prints the last two variables one after another in a joined manner.
+puts w + e
